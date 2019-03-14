0ef0edede49e9af87d6e0bc8123627c0d8f16611
unknown <pawankp550@gmail.com>
2019-03-14 08:46:05 +0530

    finished auth flow

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy('/auth/google', { target: 'https://5000-f8e88e3a-fd85-4353-b650-818de919cfb0.ws-ap0.gitpod.io' }));
+    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
 };
