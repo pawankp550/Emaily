1ac84b7ebf1e10e6f683fdcc83f39934dda15653
pawankp550 <pawankp550@gmail.com>
2019-03-18 06:02:26 +0000

    React router Phase 1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
+
+
+const Landing = () => {
+    return(
+        <h1> Landing</h1>
+    )
+}
+
+const Survey = () => {
+    return(
+        <h1> Survey</h1>
+    )
+}
+
+const SurveyNew = () => {
+    return(
+        <h1> Survey New</h1>
+    )
+}
 
 const App = () => {
 
-    return (<div>Hello</div>)
+    return (
+    <div>
+        <BrowserRouter>
+            <div>
+                 <Header />
+                <Route exact path='/' component = {Landing} />
+                <Route exact path='/surveys' component = {Survey} />
+                <Route path='/surveys/new' component = {SurveyNew} />
+            </div>
+        </BrowserRouter>
+    </div>
+    )
 
 }
 
